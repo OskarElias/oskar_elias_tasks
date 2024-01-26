c8de5175800e5becaa57bc8f90b48e5eb89edff3
OskarElias <oskar23000@student.hamk.fi>
2024-01-26 10:59:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# oskar_elias_tasks
+ PHP tasks for Web Programming course 2024
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Index</title>
+</head>
+<body>
+	<h1>Epic PHP Info</h1>
+	<?php
+		echo "<h1>hello world this is test</h1>";
+		phpinfo();
+	?>
+</body>
+</html>
